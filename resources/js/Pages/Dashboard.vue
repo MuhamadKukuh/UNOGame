37d1e09d5bf7fb8d18eb154b583da838a5b45817
MuhamadKukuh <mkukuhsetiadi@gmail.com>
2022-06-06 20:11:07 +0700

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,61 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Jetstream/Welcome.vue';
+// import { Swal } from '../../../public/vendor/sweetalert/sweetalert.all';
+
+function Alert(){
+const swalWithBootstrapButtons = Aura.mixin({
+  customClass: {
+    confirmButton: 'm-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded',
+    cancelButton: 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+  },
+  buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: 'Yes, delete it!',
+  cancelButtonText: 'No, cancel!',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    swalWithBootstrapButtons.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire(
+      'Cancelled',
+      'Your imaginary file is safe :)',
+      'error'
+    )
+  }
+})
+}
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <button @click="Alert()" class="bg-blue-500 p-4">Halo</button>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
